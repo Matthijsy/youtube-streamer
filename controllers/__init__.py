d97d8f60255462dfc85d2db5def87ad5e1c666bf
matthijs <vos1996@gmail.com>
2021-05-02 22:14:33 +0200

    OBS add audio interface

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,4 +1,3 @@
 from .obs import OBS
 from .lcd import LCD
 from .acpi import ACPI
-from .timeout import Timeout
--- controllers/obs.py
+++ controllers/obs.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from obswebsocket import obsws, requests
 
 
@@ -27,6 +29,46 @@ class OBS:
 
     def set_scene(self, name):
         res = self._call(requests.SetCurrentScene(name))
+
+        return res.status
+
+    def get_audio_volume(self, name):
+        res = self._call(requests.GetVolume(name))
+
+        return res.getVolume()
+
+    def audio_fade_out(self, name, transition_time=2):
+        volume = self.get_audio_volume(name)
+        steps = volume / transition_time / 10
+
+        for i in range(transition_time * 10):
+            res_volume = volume - ((i + 1) * steps)
+            self._call(requests.SetVolume(name, res_volume, False))
+            sleep(0.1)
+
+        return self.audio_mute(name) and self.get_audio_volume(name) == 0
+
+    def audio_fade_in(self, name, transition_time=2):
+        if not self.audio_unmute(name):
+            return False
+
+        volume = self.get_audio_volume(name)
+        steps = (1 - volume) / transition_time / 10
+
+        for i in range(transition_time * 10):
+            res_volume = volume + ((i + 1) * steps)
+            self._call(requests.SetVolume(name, res_volume, False))
+            sleep(0.1)
+
+        return self.get_audio_volume(name) == 1
+
+    def audio_mute(self, name):
+        res = self._call(requests.SetMute(name, True))
+
+        return res.status
+
+    def audio_unmute(self, name):
+        res = self._call(requests.SetMute(name, False))
 
         return res.status
 
